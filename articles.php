8c5fc3fb22e8f27cf5cd6e640e4004a65cc6ab18
Klegacy <dominikkingdiepie@gmail.com>
2020-06-07 01:45:32 -0500

    add favicon

--- articles.php
+++ articles.php
@@ -4,6 +4,9 @@
     <title>
             RBY2k20: Guides
         </title>
+        <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="96x96" href="favicon-96x96.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png">
         <!-- Required meta tags -->
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
