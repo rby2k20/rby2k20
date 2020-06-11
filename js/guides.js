834fef0ced94241a018d9b0071548a215fe0e5c4
Klegacy <dominikkingdiepie@gmail.com>
2020-06-10 21:30:15 -0500

    new loading

--- js/guides.js
+++ js/guides.js
@@ -10,33 +10,57 @@ class guide {
     content = ""
     sticky = false;
 
-    constructor(url)
+    constructor(data, url)
     {
-        
-        
-        fetch(url + ".html")
-        .then(response => response.text())
-        .then((data) => {
-            try
-            {
-                this.filename = url;
-                var doc = new DOMParser().parseFromString(data, "text/html");
-                this.title = doc.getElementsByTagName("title")[0].innerHTML;
-                this.author = doc.getElementsByTagName("author")[0].innerHTML;
-                this.day = doc.getElementsByTagName("pubday")[0].innerHTML;
-                this.month = doc.getElementsByTagName("pubmonth")[0].innerHTML;
-                this.year = doc.getElementsByTagName("pubyear")[0].innerHTML;
-                this.category = doc.getElementsByTagName("category")[0].innerHTML;
-                this.sticky = doc.getElementsByTagName("sticky")[0].innerHTML == 'true';
-                this.content = doc.getElementsByTagName("article")[0].innerHTML;
-            }
-            catch (Exception)
-            {
-                console.log(url+ " has invalid syntax");
-                console.log(Exception);
-            }         
-        });
-
-
+        try
+        {
+            this.filename = url;
+            var doc = new DOMParser().parseFromString(data, "text/html");
+            this.title = doc.getElementsByTagName("title")[0].innerHTML;
+            this.author = doc.getElementsByTagName("author")[0].innerHTML;
+            this.day = doc.getElementsByTagName("pubday")[0].innerHTML;
+            this.month = doc.getElementsByTagName("pubmonth")[0].innerHTML;
+            this.year = doc.getElementsByTagName("pubyear")[0].innerHTML;
+            this.category = doc.getElementsByTagName("category")[0].innerHTML;
+            this.sticky = doc.getElementsByTagName("sticky")[0].innerHTML == 'true';
+            this.content = doc.getElementsByTagName("article")[0].innerHTML; 
+        }
+        catch (Exception)
+        {
+            console.log(url+ " has invalid syntax");
+            console.log(Exception);
+            document.getElementById("main").innerHTML = Exception;
+        }   
     }
 }
+
+
+function getArticle(url)
+{
+    jQuery.get( url+".html", function( data ) 
+    {
+        content = new guide(data, url);
+    });
+}
+
+function pushArticle(url)
+{
+    jQuery.get( url+".html", function( data ) 
+    {
+        unsortedArticles.push(new guide(data, url));
+    });
+}
+
+//function getArticle(url)
+//{
+//    var oReq = new XMLHttpRequest();
+//    oReq.addEventListener("load", response);
+//    oReq.myURL = url;
+//    oReq.open("GET", url+ ".html");
+//    oReq.send();
+//}
+
+//function response(evt)
+//{
+//    content = new guide(this.responseText, evt.currentTarget.myURL)
+//}
--- poke.php
+++ poke.php
@@ -23,7 +23,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://code.jquery.com/jquery-3.2.1.min.js" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
         
@@ -415,24 +415,27 @@
             window.addEventListener('load', function () 
             {
                 openTab(this, meta);
-                console.log('Analyses/rbyou/' + mon + ".html");
-                fetch('Analyses/rbyou/' + mon + ".html")
-                    .then(response => {if(response.ok) return response.text(); else return "<p>No analysis found! Maybe head over to the forum and write one? :)</p>"})
-                    .then((data) => {
-                            document.getElementsByClassName("analysis")[0].innerHTML = data;
-                    });
+                jQuery.get( 'Analyses/rbyou/' + mon +".html", function( data ) 
+                {
+                    console.log(data);
+                    document.getElementsByClassName("analysis")[0].innerHTML = data;
+                }).fail(function(){ 
+                    document.getElementsByClassName("analysis")[0].innerHTML = "No analysis found! Maybe head over to the forum and write one? :)";
+                });
 
-                fetch('Analyses/rby1u/' + mon + ".html")
-                    .then(response => {if(response.ok) return response.text(); else return "<p>No analysis found! Maybe head over to the forum and write one? :)</p>"})
-                    .then((data) => {
-                            document.getElementsByClassName("analysis")[1].innerHTML = data;
-                    });
+                jQuery.get( 'Analyses/rby1u/' + mon +".html", function( data ) 
+                {
+                    document.getElementsByClassName("analysis")[1].innerHTML = data;
+                }).fail(function(){ 
+                    document.getElementsByClassName("analysis")[1].innerHTML = "No analysis found! Maybe head over to the forum and write one? :)";
+                });
 
-                fetch('Analyses/violet/' + mon + ".html")
-                    .then(response => {if(response.ok) return response.text(); else return "<p>No analysis found! Maybe head over to the forum and write one? :)</p>"})
-                    .then((data) => {
-                            document.getElementsByClassName("analysis")[2].innerHTML = data;
-                    });
+                jQuery.get( 'Analyses/violet/' + mon +".html", function( data ) 
+                {
+                    document.getElementsByClassName("analysis")[2].innerHTML = data;
+                }).fail(function(){ 
+                    document.getElementsByClassName("analysis")[2].innerHTML = "No analysis found! Maybe head over to the forum and write one? :)";
+                });
                 
             });
 
